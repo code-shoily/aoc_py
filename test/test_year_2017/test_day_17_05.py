e110d68eb361ff5aebf5b6d71db4389fafc6030b
Mafinar K <mafinar@gmail.com>
2024-03-03 23:34:27 -0500

    Solve 2017/5

---
+++ test/test_year_2017/test_day_17_05.py
@@ -0,0 +1,8 @@
+import pytest
+from year_2017.day_17_05 import get_input_data, run_17_5
+
+
+@pytest.mark.slow
+def test_day_17_5_works():
+    parsed_input = get_input_data()
+    assert run_17_5(parsed_input) == (372_671, 25_608_480)
