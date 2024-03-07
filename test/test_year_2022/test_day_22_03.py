7504d8bae4708b954a28523ab633821a5b0d7932
Mafinar K <mafinar@gmail.com>
2024-03-06 19:21:31 -0500

    Add tests to 22/3

--- test/test_year_2022/test_day_22_03.py
+++ test/test_year_2022/test_day_22_03.py
@@ -3,4 +3,4 @@ from year_2022.day_22_03 import get_input_data, run_22_3
 
 def test_day_22_3_works():
     parsed_input = get_input_data()
-    assert run_22_3(parsed_input) == (0, 0)
+    assert run_22_3(parsed_input) == (8233, 2821)
