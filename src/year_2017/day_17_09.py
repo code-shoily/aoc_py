4c53c5a490925b7c013161c0c231ed9692c42ad2
Mafinar K <mafinar@gmail.com>
2024-03-09 01:06:08 -0500

    Add tags to 17/9

--- src/year_2017/day_17_09.py
+++ src/year_2017/day_17_09.py
@@ -1,7 +1,7 @@
 """Advent of Code Year 2017, Day 9 - Stream Processing
 Problem Link: https://adventofcode.com/2017/day/9
 Difficulty: S
-Tags:
+Tags: state-machine
 """
 
 from dataclasses import dataclass
